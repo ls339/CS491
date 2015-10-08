abeae5fb5d6447d309c3afda7fa165ab0f751137
LouSoto <ls339@njit.edu>
2015-10-07 22:37:00 -0400

    Kick off of DH

--- src/cs491/DiffieHellman.java
+++ src/cs491/DiffieHellman.java
@@ -1,6 +1,31 @@
 package cs491;
 
+import java.io.*;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.spec.*;
+import java.security.interfaces.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import javax.crypto.interfaces.*;
+import com.sun.crypto.provider.SunJCE;
+
 public class DiffieHellman {
+	
+	private BigInteger p, g;
+	
+	// https://docs.oracle.com/javase/7/docs/api/javax/crypto/spec/DHParameterSpec.html
+	DHParameterSpec params = new DHParameterSpec(p,g);
+	
+	// http://docs.oracle.com/javase/7/docs/api/java/security/KeyPairGenerator.html
+	KeyPairGenerator requestorGen = KeyPairGenerator.getInstance("DH");
+	
+	requestorGen.initialize(params, new SecureRandom());
+	
+	public DiffieHellman() {
+		//Testing 
+		System.out.println(Security.getProviders());
+	}
 
 }
 
