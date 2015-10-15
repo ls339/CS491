2a320bdb76696bc113e53ff1e4d0e9531e84ce1b
AlexXi <xi.alex.t@gmail.com>
2015-10-14 21:30:37 -0400

    Added descriptions for Pseudo Code
    Added details for our pseudo code submission. Left out some key parts,
    work with team to fill in later.

--- src/cs491/PseudoApp1.java
+++ src/cs491/PseudoApp1.java
@@ -8,17 +8,24 @@ public class PseudoApp1 {
 	 * beginSession() {
 	 * 
 	 * Resolve - Saad and Alex
-	 * requestorModulo <- providerGetModulo() 
-	 * or
-	 * modulo <- inputModulo()
+	 * Previously agreed upon primeNumber & primeModulo
+	 * primeNumber ^ [privateKey] mod primeModulo = PublicKey(s)
+	 * PublicKey ^ [privateKey] mod primeModulo	= SharedKey					//////modulo <- inputModulo()//////
 	 * 
-	 * randomNumber <- rand()
+	 * 																		//////randomNumber <- rand()//////
 	 * 
-	 * GenerateKeypair()
-	 * 
-	 * publicKeyEncrypt()
-	 * 
-	 * 
+	 *								
+	 * ProviderGenerateKeyPair()	//Provider generates public and private key
+	 * 								//Provider sends public key to Requester					//DOF takes object with keys
+	 * 								//Requester receives public key								//DOF sends object with keys
+	 * RequesterGenerateKeyPair		//Requester generates private key
+	 * 								//Requester sends public key to Provider					//DOF sends object with key
+	 *								//Both parties generate shared secret						//Diffie-Hellman achieved
+	 * 																							//Parties begin Encryption
+	 *								//Data Transformation occurs for all sent and received data
+	 *								//Shared secret is used to transform future data			//Encryption and Decryption
+	 *
+	 *								//Requester ends the session
 	 * }
 	 */
 	
