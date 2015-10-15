cfe2f22cf792b56cd9e64a47e8c08c109716f0a4
AlexXi <AlexXi@users.noreply.github.com>
2015-10-14 21:35:03 -0400

    corrected formatting issues

--- src/cs491/PseudoApp1.java
+++ src/cs491/PseudoApp1.java
@@ -10,22 +10,22 @@ public class PseudoApp1 {
 	 * Resolve - Saad and Alex
 	 * Previously agreed upon primeNumber & primeModulo
 	 * primeNumber ^ [privateKey] mod primeModulo = PublicKey(s)
-	 * PublicKey ^ [privateKey] mod primeModulo	= SharedKey					//////modulo <- inputModulo()//////
+	 * PublicKey ^ [privateKey] mod primeModulo	= SharedKey			//removable//modulo <- inputModulo()
 	 * 
-	 * 																		//////randomNumber <- rand()//////
+	 * 										//removable//randomNumber <- rand()
 	 * 
 	 *								
 	 * ProviderGenerateKeyPair()	//Provider generates public and private key
-	 * 								//Provider sends public key to Requester					//DOF takes object with keys
-	 * 								//Requester receives public key								//DOF sends object with keys
-	 * RequesterGenerateKeyPair		//Requester generates private key
-	 * 								//Requester sends public key to Provider					//DOF sends object with key
-	 *								//Both parties generate shared secret						//Diffie-Hellman achieved
-	 * 																							//Parties begin Encryption
-	 *								//Data Transformation occurs for all sent and received data
-	 *								//Shared secret is used to transform future data			//Encryption and Decryption
+	 * 				//Provider sends public key to Requester		//DOF takes object with keys
+	 * 				//Requester receives public key				//DOF sends object with keys
+	 * RequesterGenerateKeyPair	//Requester generates private key
+	 * 				//Requester sends public key to Provider		//DOF sends object with key
+	 *				//Both parties generate shared secret			//Diffie-Hellman achieved
+	 * 											//Parties begin Encryption
+	 *				//Data Transformation occurs for all sent and received data
+	 *				//Shared secret is used to transform future data	//Encryption and Decryption
 	 *
-	 *								//Requester ends the session
+	 *				//Requester ends the session
 	 * }
 	 */
 	
