ebeb70b26758983721ab77be404f75dfde7b1728
LouSoto <ls339@njit.edu>
2015-10-14 21:00:26 -0400

    saved file

--- src/cs491/PseudoApp1.java
+++ src/cs491/PseudoApp1.java
@@ -1,5 +1,26 @@
 package cs491;
 
 public class PseudoApp1 {
+	/* 
+	 * Input: DOF interface is the default interface, end2endsec is the interface type  
+	 * Output: A session object providing a secure end-to-end connection
+	 * 
+	 * beginSession() {
+	 * 
+	 * Resolve - Saad and Alex
+	 * requestorModulo <- providerGetModulo() 
+	 * or
+	 * modulo <- inputModulo()
+	 * 
+	 * randomNumber <- rand()
+	 * 
+	 * GenerateKeypair()
+	 * 
+	 * publicKeyEncrypt()
+	 * 
+	 * 
+	 * }
+	 */
+	
 
 }
