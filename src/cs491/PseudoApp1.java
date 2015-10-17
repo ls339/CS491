02d1b7a31088ad9d452e0d581f1be137199e710a
ls339 <ls339@njit.edu>
2015-10-16 22:41:38 -0400

    Lou's contrib

--- src/cs491/PseudoApp1.java
+++ src/cs491/PseudoApp1.java
@@ -1,30 +1,26 @@
-  Interface: End-to-end interface
-      endToEndInterface() {
-
-      DOFinterface EndToEnd
-      DOFinterface.ID <- DOFinterfaceID.create()
-      EndToEnd.builder(IID)
-
+Interface: End-to-end interface
+	endToEndInterface() {
+      	DOFinterface EndToEnd
+      	DOFinterface.ID <- DOFinterfaceID.create()
+      	EndToEnd.builder(IID)
 	// Do some secure things in here
-
   }
 	
-  Input: DOF interface is the default interface, end2endsec is the interface type  
-  Output: A session object providing a secure end-to-end connection
-  Function: Sets up an end-to-end secure session 
-  beginSession() {
-  
-  Previously agreed upon primeNumber & primeModulo
-  primeNumber ^ [privateKey] mod primeModulo = PublicKey(s)
-  PublicKey ^ [privateKey] mod primeModulo	= SharedKey			
-  
-  	
-  
- 								
-  ProviderGenerateKeyPair()	//Provider generates public and private key
-  				//Provider sends public key to Requester		//DOF takes object with keys
+Input: DOF interface is the default interface, end2endsec is the interface type  
+Output: A session object providing a secure end-to-end connection
+Function: Sets up an end-to-end secure session 
+  	beginSession() {
+  		// Assume begin provide on the other end has 
+  		Previously agreed upon primeNumber & primeModulo
+  		primeNumber ^ [privateKey] mod primeModulo = PublicKey(s)
+  		PublicKey ^ [privateKey] mod primeModulo	= SharedKey			
+  		ProviderGenerateKeyPair()	//Provider generates public and private key
+  		DOFSendPubKeyToProvider() 
+  				/* Provider sends public key to Requester
+  				 * DOF takes object with keys
+  				 */
   				//Requester receives public key				//DOF sends object with keys
-  RequesterGenerateKeyPair	//Requester generates private key
+  		RequesterGenerateKeyPair	//Requester generates private key
   				//Requester sends public key to Provider		//DOF sends object with key
  				//Both parties generate shared secret			//Diffie-Hellman achieved
   											//Parties begin Encryption
@@ -32,7 +28,6 @@
  				//Shared secret is used to transform future data	//Encryption and Decryption
  
  				//Requester ends the session
-  return End2EndSessionOject
   }
   
 Input: End2EndSessionOject
@@ -43,7 +38,15 @@ Function: Destroys an end-to-end connection
         destroy(End2EndSessionOject)
     }
 
-
- 
+/* Test app using our interface */
+Input: integer n which represents time, DOFObject
+Output: integer r ; 0 = success, 1 = failure
+setAlarmTime() {
+	DOFObject.beginSession()
+	DOFObject.SendSetRequest(n)
+		if exception
+			throws exception
+	not throw exception
+}
 	
 
