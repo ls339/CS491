4ec2ef4fb8de8f73ef6f7b629273a03f6a558ce7
LouSoto <ls339@njit.edu>
2015-10-16 21:54:12 -0400

    Added Interface

--- src/cs491/PseudoApp1.java
+++ src/cs491/PseudoApp1.java
@@ -1,33 +1,49 @@
-package cs491;
+  Interface: End-to-end interface
+      endToEndInterface() {
 
-public class PseudoApp1 {
-	/* 
-	 * Input: DOF interface is the default interface, end2endsec is the interface type  
-	 * Output: A session object providing a secure end-to-end connection
-	 * 
-	 * beginSession() {
-	 * 
-	 * Resolve - Saad and Alex
-	 * Previously agreed upon primeNumber & primeModulo
-	 * primeNumber ^ [privateKey] mod primeModulo = PublicKey(s)
-	 * PublicKey ^ [privateKey] mod primeModulo	= SharedKey			//removable//modulo <- inputModulo()
-	 * 
-	 * 										//removable//randomNumber <- rand()
-	 * 
-	 *								
-	 * ProviderGenerateKeyPair()	//Provider generates public and private key
-	 * 				//Provider sends public key to Requester		//DOF takes object with keys
-	 * 				//Requester receives public key				//DOF sends object with keys
-	 * RequesterGenerateKeyPair	//Requester generates private key
-	 * 				//Requester sends public key to Provider		//DOF sends object with key
-	 *				//Both parties generate shared secret			//Diffie-Hellman achieved
-	 * 											//Parties begin Encryption
-	 *				//Data Transformation occurs for all sent and received data
-	 *				//Shared secret is used to transform future data	//Encryption and Decryption
-	 *
-	 *				//Requester ends the session
-	 * }
-	 */
+      DOFinterface EndToEnd
+      DOFinterface.ID <- DOFinterfaceID.create()
+      EndToEnd.builder(IID)
+
+	// Do some secure things in here
+
+  }
+	
+  Input: DOF interface is the default interface, end2endsec is the interface type  
+  Output: A session object providing a secure end-to-end connection
+  Function: Sets up an end-to-end secure session 
+  beginSession() {
+  
+  Previously agreed upon primeNumber & primeModulo
+  primeNumber ^ [privateKey] mod primeModulo = PublicKey(s)
+  PublicKey ^ [privateKey] mod primeModulo	= SharedKey			
+  
+  	
+  
+ 								
+  ProviderGenerateKeyPair()	//Provider generates public and private key
+  				//Provider sends public key to Requester		//DOF takes object with keys
+  				//Requester receives public key				//DOF sends object with keys
+  RequesterGenerateKeyPair	//Requester generates private key
+  				//Requester sends public key to Provider		//DOF sends object with key
+ 				//Both parties generate shared secret			//Diffie-Hellman achieved
+  											//Parties begin Encryption
+ 				//Data Transformation occurs for all sent and received data
+ 				//Shared secret is used to transform future data	//Encryption and Decryption
+ 
+ 				//Requester ends the session
+  return End2EndSessionOject
+  }
+  
+Input: End2EndSessionOject
+Output: void
+Function: Destroys an end-to-end connection
+
+    killSession() {
+        destroy(End2EndSessionOject)
+    }
+
+
+ 
 	
 
-}
