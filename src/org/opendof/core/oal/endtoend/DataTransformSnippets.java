6e61df9904c674fddcc12c7a5575da230fa3a025
AlexXi <xi.alex.t@gmail.com>
2015-11-13 18:27:03 -0500

    more notes and snippet update

--- src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
+++ src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
@@ -1,33 +1,32 @@
 //Cipher input stream, cipher output streams instead of byte[]
 
+Cipher aesEncryptCipher = Cipher.getInstance("AES");
+Cipher aesDecryptCipher = Cipher.getInstance("AES"); 
+AesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret);
+AesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret); //TODO find out if ant of this can be merged
+
+@Override
 public transformSendData(DOFInterfaceID interfaceID, byte[] data) 
 {
 	//receiverSharedSecret generated outside this method
 	SecretKey sharedSecret = receiverSharedSecret; //change to provider if provider
 	
-	Cipher AesCipher = Cipher.getInstance("AES");
-	
-	byte[] byteData = data.getBytes();
-	
-	AesCipher.init(Cipher.ENCRYPT_MODE, sharedSecret);
-	byte[] byteCipherData = AesCipher.doFinal(byteData);
+	//byte[] byteData = data.getBytes();
+	byte[] byteCipherData = aesEncryptCipheresCipher.doFinal(data);
 	
 	//now send the cipher text across the session (This occurs outside this method)
 	return byteCipherData;
 }
 
+@Override
 public transformReceiveData(DOFInterfaceID interfaceID, byte[] data) 
 {
 	//receiverSharedSecret generated outside this method
 	SecretKey sharedSecret = receiverSharedSecret; //change to provider if provider
 	
-	Cipher AesCipher = Cipher.getInstance("AES");
+	//byte[] byteCipherData = data.getBytes();
+	byte[] bytePlainData = aesDecryptCipher.doFinal(data);
 	
-	byte[] byteCipherData = data.getBytes();
-	
-	AesCipher.init(Cipher.DECRYPT_MODE, sharedSecret);
-	byte[] bytePlainData = AesCipher.doFinal(byteCipherData);
-	
-	//now send the decrypted data back to application
+	//now send the decrypted data back to application (find out where this occurs)
 	return bytePlainData;
 }
