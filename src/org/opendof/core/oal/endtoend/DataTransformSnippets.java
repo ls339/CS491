7f0f53c9a1611e96a3cd0db150a2edd63e8a2661
AlexXi <xi.alex.t@gmail.com>
2015-11-13 11:24:41 -0500

    added data transform snippets
    curently incomplete

---
+++ src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
@@ -0,0 +1,31 @@
+public transformSendData(DOFInterfaceID interfaceID, byte[] data) 
+{
+	//receiverSharedSecret generated outside this method
+	SecretKey sharedSecret = receiverSharedSecret; //change to provider if provider
+	
+	Cipher AesCipher = Cipher.getInstance("AES");
+	
+	byte[] byteData = data.getBytes();
+	
+	AesCipher.init(Cipher.ENCRYPT_MODE, sharedSecret);
+	byte[] byteCipherData = AesCipher.doFinal(byteData);
+	
+	//now send the cipher text across the session (This occurs outside this method)
+	return byteCipherData;
+}
+
+public transformReceiveData(DOFInterfaceID interfaceID, byte[] data) 
+{
+	//receiverSharedSecret generated outside this method
+	SecretKey sharedSecret = receiverSharedSecret; //change to provider if provider
+	
+	Cipher AesCipher = Cipher.getInstance("AES");
+	
+	byte[] byteCipherData = data.getBytes();
+	
+	AesCipher.init(Cipher.DECRYPT_MODE, sharedSecret);
+	byte[] bytePlainData = AesCipher.doFinal(byteCipherData);
+	
+	//now send the decrypted data back to application
+	return bytePlainData;
+}
