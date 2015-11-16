a5a6fd666f301f6df1cfb18ea50c14cfff92c83d
LouSoto <ls339@njit.edu>
2015-11-16 18:27:35 -0500

    new methods

---
+++ src/org/opendof/core/oal/endtoend/ETEInterface.java
@@ -0,0 +1,58 @@
+package org.opendof.core.oal.endtoend;
+
+import org.opendof.core.oal.DOFInterface;
+import org.opendof.core.oal.DOFInterfaceID;
+import org.opendof.core.oal.DOFType;
+import org.opendof.core.oal.value.DOFBlob;
+import org.opendof.core.oal.value.DOFBoolean;
+import org.opendof.core.oal.value.DOFDateTime;
+import java.math.BigInteger;
+import java.security.KeyPair;
+
+import javax.crypto.spec.*;
+
+public class ETEInterface {
+
+	public static final DOFBlob.Type BLOB_KEY = new DOFBlob.Type(32, 32);
+	
+	public static final DOFInterface DEF;
+	public static final DOFInterfaceID IID = DOFInterfaceID.create("[63:{53551070}]");
+
+    public static final int PROPERTY_BLOB_KEY_ID = 1;
+    public static final int METHOD_SEND_ENCODED_PUB_KEY_ID = 2;
+    //public static final int METHOD_GEN_SHARED_SECRET_ID = 3; // Not needed
+    //public static final int METHOD_DATA_TRANSFORM_ID = 4; // Not needed
+    
+    /*
+	public static final DOFInterface.Property PROPERTY_ALARM_ACTIVE;
+	public static final DOFInterface.Property PROPERTY_ALARM_TIME_VALUE;
+	*/
+	/*
+	 * split this into two methods
+	 * (Method 1)
+	 * public static final DOFInterface.Method METHOD_KEY_PAIR_GEN; // end-to-end should return a keypair object
+	 * public static final DOFInterface.Method INIT_KEY_AGREEMENT;
+	 * public static final DOFInterface.Method ENCODE_PUB_KEY; // Send key up to encode
+	 * (Method 2)
+	 * public static final DOFInterface.Method DECODE_PUB_KEY; // return public key
+	 * public static final DOFInterface.Method GET_DH_PARAM_SPECS;
+	 * public static final DOFInterface.Method KEY_AGREEMENT_DO_PHASE;
+	 * public static final DOFInterface.Method GEN_SHARED_SECRET;
+	 */
+
+	public static final DOFInterface.Method SEND_ENCODED_PUB_KEY; // Method 1
+	//public static final DOFInterface.Method GEN_SHARED_SECRET; // Method 2
+	//public static final DOFInterface.Method DATA_TRANSFORM; 
+	
+	static {
+    	DEF = new DOFInterface.Builder(IID)
+    			.addProperty(1, true, true, BLOB_KEY)
+    			.addMethod(2, new DOFType[] { BLOB_KEY },new DOFType[] { BLOB_KEY }).build();
+    			//.addMethod(3, null, null)
+    			//.addMethod(4, new DOFType[] { BLOB_KEY }, null).build();
+
+    	SEND_ENCODED_PUB_KEY = DEF.getMethod(METHOD_SEND_ENCODED_PUB_KEY_ID);
+    	//GEN_SHARED_SECRET = DEF.getMethod(METHOD_GEN_SHARED_SECRET_ID);
+    	//DATA_TRANSFORM = DEF.getMethod(METHOD_DATA_TRANSFORM_ID);
+	}
+}
---
+++ src/org/opendof/core/oal/endtoend/Provider.java
@@ -0,0 +1,181 @@
+package org.opendof.core.oal.endtoend;
+
+import java.util.Date;
+import java.util.List;
+
+import org.opendof.core.oal.DOFNotSupportedException;
+import org.opendof.core.oal.DOFObject;
+import org.opendof.core.oal.DOFObjectID;
+import org.opendof.core.oal.DOFSystem;
+import org.opendof.core.oal.DOFType;
+import org.opendof.core.oal.DOFValue;
+import org.opendof.core.oal.DOFInterface.Method;
+import org.opendof.core.oal.DOFInterface.Property;
+import org.opendof.core.oal.DOFOperation.Provide;
+import org.opendof.core.oal.value.DOFBoolean;
+import org.opendof.core.oal.DOFRequest;
+
+public class Provider {
+
+    DOFSystem mySystem = null;
+    DOFObject myObject = null;
+    DOFObjectID myOID = null;
+    String lastOp = "undefined";
+    int delay = 0;
+    
+    boolean isActive = false;
+    Date alarmTime;
+    
+    public Provider(DOFSystem _system, String oidString){
+        mySystem = _system;
+        init(oidString);
+    }
+    
+    private void init(String _oidString){
+        myOID = DOFObjectID.create(_oidString);
+        myObject = mySystem.createObject(myOID);
+        myObject.beginProvide(TBAInterface.DEF, new ProviderListener()); 
+    }
+    
+    public boolean getActive(){
+        return isActive;
+    }
+    
+    public void setActive(boolean _active){
+        isActive = _active;
+    }
+    
+    public Date getAlarmTime(){
+        return alarmTime;
+    }
+    
+    public void setAlarmTime(Date _alarmTime){
+        alarmTime = _alarmTime;
+    }
+    
+    public String getLastOperation(){
+        if(lastOp!=null)
+            return lastOp;
+        else
+            return "undefined";
+    }
+    
+    public void setDelay(int _delay){
+        delay = _delay;
+    }
+    
+    private class ProviderListener extends DOFObject.DefaultProvider {
+    	/*
+         * Trap BeginSession here
+         * Override DefaultProvider to trap session callback
+         * Intercept begin provide
+         * if(end-to-end security was requested) then
+       	 * 		Save original interface DEF requested
+         *		Provide end-to-end secure beginProvide
+         * 		myObject.beginProvide(end-to-end security);
+         *		DH key negotiation
+         * 		Return end-to-end secured session object with original interface request.
+         * else 
+         * Let OpenDOF handle the session 
+         * 
+         * JS mentioned that we can copy one of the below methods to use for our function.
+         * 
+         * 
+         * Add a session call 
+        @Override
+        public void get(Provide operation, DOFRequest.Get request, Property property) {
+            DOFBoolean myDOFBoolean = new DOFBoolean(isActive);
+            
+            if(delay > 0){
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                	request.respond(new DOFNotSupportedException("The specified item id is not supported by the specified interface."));
+                    //Specific exceptions are now available and the constructor below is deprecated.
+                    //request.respond(new DOFErrorException(DOFErrorException.APPLICATION_ERROR));
+                }
+            }
+            
+            request.respond(myDOFBoolean);
+            lastOp = "get";
+        }
+         */
+        @Override
+        public void get(Provide operation, DOFRequest.Get request, Property property) {
+            DOFBoolean myDOFBoolean = new DOFBoolean(isActive);
+            
+            if(delay > 0){
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                	request.respond(new DOFNotSupportedException("The specified item id is not supported by the specified interface."));
+                    //Specific exceptions are now available and the constructor below is deprecated.
+                    //request.respond(new DOFErrorException(DOFErrorException.APPLICATION_ERROR));
+                }
+            }
+            
+            request.respond(myDOFBoolean);
+            lastOp = "get";
+        }
+        
+        @Override
+        public void set(Provide operation, DOFRequest.Set request, Property property, DOFValue value) {
+            isActive = DOFType.asBoolean(value);
+            
+            if(delay > 0){
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                	request.respond(new DOFNotSupportedException("The specified item id is not supported by the specified interface."));
+                    //Specific exceptions are now available and the constructor below is deprecated.
+                    //request.respond(new DOFErrorException(DOFErrorException.APPLICATION_ERROR));
+                }
+            }
+            
+            request.respond();
+            lastOp = "set";
+        }
+        
+        @Override
+        public void invoke(Provide operation, DOFRequest.Invoke request, Method method, List<DOFValue> parameters) {
+            alarmTime = DOFType.asDate(parameters.get(0));
+            
+            DOFBoolean myDOFBoolean = new DOFBoolean(isActive);
+            
+            if(delay > 0){
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                	request.respond(new DOFNotSupportedException("The specified item id is not supported by the specified interface."));
+                    //Specific exceptions are now available and the constructor below is deprecated.
+                    //request.respond(new DOFErrorException(DOFErrorException.APPLICATION_ERROR));
+                }
+            }
+            
+            request.respond(myDOFBoolean);
+            lastOp = "invoke";
+        }
+        @Override
+        public void invoke(Provide operation, DOFRequest.Invoke request, Method method, List<DOFValue> parameters) {
+            // Figure out what method is being called if we have more than one method.
+        	
+        	byte[] key = DOFType.asBytes(parameters.get(0));
+            
+            // Do app logic
+            DOFBoolean myDOFBoolean = new DOFBoolean(isActive);
+            
+            if(delay > 0){
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                	request.respond(new DOFNotSupportedException("The specified item id is not supported by the specified interface."));
+                    //Specific exceptions are now available and the constructor below is deprecated.
+                    //request.respond(new DOFErrorException(DOFErrorException.APPLICATION_ERROR));
+                }
+            }
+            
+            request.respond(myDOFBoolean); // respond with another blob
+            lastOp = "invoke";
+        }
+    }
+}
