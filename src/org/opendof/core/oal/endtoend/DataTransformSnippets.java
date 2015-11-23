3f026370cc0249fc173263bc8d8344cb13222b2d
AlexXi <AlexXi@users.noreply.github.com>
2015-11-23 16:49:16 -0500

    fixed issues
    including return types and other minutiae.
    
    problems still persist with how to collect saved data and how to read/write/doFinal()

--- src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
+++ src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
@@ -1,75 +1,90 @@
 //Cipher input stream, cipher output streams instead of byte[]
 
 /*
-TODO --------> implement data transform interface: dofoperation.session.datatransform
-Cipher aesEncryptCipher = Cipher.getInstance("AES");
-Cipher aesDecryptCipher = Cipher.getInstance("AES"); 
-aesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret);
-aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret); //TODO find out if this is removable
-
-workflow: est session - key neg - construct obj to gen ciphers - attach on both sides of session
+	TODO --------> implement data transform interface: dofoperation.session.datatransform
+	Cipher aesEncryptCipher = Cipher.getInstance("AES");
+	Cipher aesDecryptCipher = Cipher.getInstance("AES"); 
+	aesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret);
+	aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret);
+	
+	workflow: est session - key neg - construct obj to gen ciphers - attach on both sides of session
 */
 
 // requester - session operation returned, dofoperation.session returned when session created
 // provider - dofrequest.session created when session request received. call setdatatransform here
 
-//@Override | TODO Pass in aesDecryptCipher? | TODO pass as input shared secret? or no?
-public useCipherInputStream(ByteArrayInputStream in, Cipher aesDecryptCipher)
+public CipherInputStream useCipherInputStream(ByteArrayInputStream in, Cipher aesDecryptCipher, SecretKey sharedSecret, IvParameterSpec iv)
 {
-	sharedSecret = receiverSharedSecret;
-	aesDecryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //MUST specify an IV and distribute to both sides
-	aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
-	
-	CipherInputStream cis = new CipherInputStream(in, aesDecryptCipher);
-	
-	return cis; //TODO what do we return?
-} //TODO how to close stream?
+	try {
+		//sharedSecret = receiverSharedSecret;
+		aesDecryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //MUST specify an IV and distribute to both sides
+		aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
+		
+		CipherInputStream cis = new CipherInputStream(in, aesDecryptCipher);
+		
+		return cis; //TODO what do we return?
+	}
+	catch(Exception e) {
+		return null;
+	}
+}
 
 //create ciphers in initialized method or constructor - class level private variables
-//@Override | TODO what do we pass in?
-public useCipherOutputStream(ByteArrayOutputStream os, Cipher aesEncryptCipher)
+public CipherOutputStream useCipherOutputStream(ByteArrayOutputStream os, Cipher aesEncryptCipher, SecretKey sharedSecret, IvParameterSpec iv)
 {
-	sharedSecret = receiverSharedSecret;
-	aesDecryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	aesDecryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
-	
-	CipherOutputStream cos = new CipherOutputStream(os, aesEncryptCipher);
-	
-	return cos; //TODO what do we return?
+	try {
+		//sharedSecret = receiverSharedSecret;
+		aesEncryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
+		
+		CipherOutputStream cos = new CipherOutputStream(os, aesEncryptCipher);
+		
+		return cos; //TODO what do we return?
+	}
+	catch(Exception e) {
+		return null;
+	}
 } //TODO how to close stream?
 
+//Data transform encapsulates the cipher streams
 @Override
-public transformSendData(DOFInterfaceID interfaceID, byte[] data) //Data transform encapsulates the cipher streams
+public transformSendData(DOFInterfaceID interfaceID, byte[] data) //constructor override
 {
-	//receiverSharedSecret generated outside this method
-	//SecretKey sharedSecret = receiverSharedSecret;
-	//------------------------------------------------------------------
-	//use the cipher method to create the cipher and init AES encryption
-	CipherOutputStream cos = useCipherOutputStream(ByteArrayOutputStream os, Cipher aesEncryptCipher);
+	//receiverSharedSecret saved outside this method
+	SecretKey sharedSecret = receiverSharedSecret;
+	IvParameterSpec iv = savedIvSpec;
+	ByteArrayOutputStream os;
+	Cipher aesEncryptCipher;
+	CipherOutputStream cos;
+	try{
+		cos = useCipherOutputStream(os, aesEncryptCipher, sharedSecret, iv);
+	}
+	catch(NullPointerException e){
+		//TODO what to do if null
+	}
 	//TODO - doFinal not called with stream cipher?
 	//byte[] byteCipherData = aesEncryptCipher.doFinal(data); //convert to cipher data
-	//cos.write(byteCipherData); //write the cipher data to the cipher stream
-	//------------------------------------------------------------------
-	//now send the cipher text across the session (This occurs outside this method)
-	//return byteCipherData; //what do we return?
 	byte[] byteCipherData = cos.write(data);
 	return byteCipherData;
 }
 
 @Override
-public transformReceiveData(DOFInterfaceID interfaceID, byte[] data) 
+public transformReceiveData(DOFInterfaceID interfaceID, byte[] data) //constructor override
 {
-	//receiverSharedSecret generated outside this method
-	//SecretKey sharedSecret = receiverSharedSecret;
-	//------------------------------------------------------------------
-	//use the cipher method to create the cipher and init AES encryption
-	CipherInputStream cis = useCipherInputStream(ByteArrayInputStream os, Cipher aesDecryptCipher);
+	//receiverSharedSecret saved outside this method
+	SecretKey sharedSecret = receiverSharedSecret;
+	IvParameterSpec iv = savedIvSpec;
+	ByteArrayInputStream is;
+	Cipher aesDecryptCipher;
+	CipherInputStream cis;
+	try {
+		cis = useCipherInputStream(is, aesDecryptCipher, sharedSecret, iv);
+	}
+	catch(NullPointerException e){
+		//TODO what to do if null
+	}
 	//TODO - doFinal not called with stream Cipher??
 	//byte[] bytePlainData = aesDecryptCipher.doFinal(data); //convert cipher data to plain data
-	//cis.read(bytePlainData); //use the cipher stream to read the data
-	//------------------------------------------------------------------
-	//now send the decrypted data back to application (find out where this occurs)
-	//return bytePlainData;
-	byte[] bytePlainData = cis.read(data);
+	byte[] bytePlainData =  cis.read(data);
 	return bytePlainData;
 }
