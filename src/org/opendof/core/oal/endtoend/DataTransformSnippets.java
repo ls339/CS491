b2fb69a3e91baadbbaa497c6a717edba803d5d00
AlexXi <AlexXi@users.noreply.github.com>
2015-11-23 20:05:07 -0500

    major revert

--- src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
+++ src/org/opendof/core/oal/endtoend/DataTransformSnippets.java
@@ -13,16 +13,14 @@
 // requester - session operation returned, dofoperation.session returned when session created
 // provider - dofrequest.session created when session request received. call setdatatransform here
 
-public CipherInputStream useCipherInputStream(ByteArrayInputStream in, Cipher aesDecryptCipher, SecretKey sharedSecret, IvParameterSpec iv)
+public Cipher getDecryptCipher(Cipher inCipher, SecretKey sharedSecret, IvParameterSpec iv)
 {
 	try {
-		//sharedSecret = receiverSharedSecret;
+		Cipher aesDecryptCipher = inCipher;
 		aesDecryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //MUST specify an IV and distribute to both sides
 		aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
-		
-		CipherInputStream cis = new CipherInputStream(in, aesDecryptCipher);
-		
-		return cis; //TODO what do we return?
+
+		return aesDecryptCipher;
 	}
 	catch(Exception e) {
 		return null;
@@ -30,60 +28,32 @@ public CipherInputStream useCipherInputStream(ByteArrayInputStream in, Cipher ae
 }
 
 //create ciphers in initialized method or constructor - class level private variables
-public CipherOutputStream useCipherOutputStream(ByteArrayOutputStream os, Cipher aesEncryptCipher, SecretKey sharedSecret, IvParameterSpec iv)
+public Cipher getEncryptCipher(Cipher inCipher, SecretKey sharedSecret, IvParameterSpec iv)
 {
 	try {
-		//sharedSecret = receiverSharedSecret;
+		Cipher aesEncryptCipher = inCipher;
 		aesEncryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		aesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
-		
-		CipherOutputStream cos = new CipherOutputStream(os, aesEncryptCipher);
-		
-		return cos; //TODO what do we return?
+
+		return aesEncryptCipher;
 	}
 	catch(Exception e) {
 		return null;
 	}
-} //TODO how to close stream?
+}
 
-//Data transform encapsulates the cipher streams
 @Override
 public byte[] transformSendData(DOFInterfaceID interfaceID, byte[] data)
 {
-	//receiverSharedSecret saved outside this method
-	SecretKey sharedSecret = receiverSharedSecret;
-	IvParameterSpec iv = savedIvSpec;
-	ByteArrayOutputStream os;
-	Cipher aesEncryptCipher;
-	CipherOutputStream cos;
-
-	cos = useCipherOutputStream(os, aesEncryptCipher, sharedSecret, iv);
-	
-	byte[] byteCipherData = data;
-	cos.write(byteCipherData);
-	cos.close(); //this calls doFinal()
+	Cipher aesEncryptCipher = savedEncryptCipher;
+	byte[] byteCipherData = aesEncryptCipher.doFinal(data);
 	return byteCipherData;
 }
 
 @Override
 public byte[] transformReceiveData(DOFInterfaceID interfaceID, byte[] data)
 {
-	//receiverSharedSecret saved outside this method
-	SecretKey sharedSecret = receiverSharedSecret;
-	IvParameterSpec iv = savedIvSpec;
-	ByteArrayInputStream is;
-	Cipher aesDecryptCipher;
-	CipherInputStream cis;
-
-	cis = useCipherInputStream(is, aesDecryptCipher, sharedSecret, iv);
-
-	//TODO - doFinal not called with stream input Cipher??
-	byte[] bytePlainData;
-	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	while(cis.read(data) != -1)
-	{
-		outputStream.write(cis.read(data));
-	}
-	bytePlainData = outputStream.toByteArray();
+	Cipher aesDecryptCipher = savedDecryptCipher;
+	byte[] bytePlainData = aesDecryptCipher.doFinal(data);
 	return bytePlainData;
 }
