9b12418a0e49203ea9428a174237512c7c2be025
AlexXi <AlexXi@users.noreply.github.com>
2015-12-04 16:55:10 -0500

    Update Provider.java

--- src/org/opendof/core/oal/endtoend/Provider.java
+++ src/org/opendof/core/oal/endtoend/Provider.java
@@ -219,47 +219,47 @@ public class Provider {
     public eteDataTransform ETE_DATA_TRANSFORM = new Requestor.eteDataTransform();
     public final class eteDataTransform implements DataTransform 
     {
-    public Cipher createDecryptCipher(Cipher inCipher, SecretKey sharedSecret, IvParameterSpec iv)
-    {
-    	try {
-    		Cipher aesDecryptCipher = inCipher;
-    		aesDecryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //MUST specify an IV and distribute to both sides
-    		aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
-    
-    		return aesDecryptCipher;
-    	}
-    	catch(Exception e) {
-    		return null;
-    	}
-    }
-    //create ciphers in initialized method or constructor - class level private variables
-    public Cipher createEncryptCipher(Cipher inCipher, SecretKey sharedSecret, IvParameterSpec iv)
-    {
-    	try {
-    		Cipher aesEncryptCipher = inCipher;
-    		aesEncryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-    		aesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
-    
-    		return aesEncryptCipher;
-    	}
-    	catch(Exception e) {
-    		return null;
-    	}
-    }
-    
-    public byte[] transformSendData(DOFInterfaceID interfaceID, byte[] data)
-    {
-    	Cipher aesEncryptCipher = savedEncryptCipher;
-    	byte[] byteCipherData = aesEncryptCipher.doFinal(data);
-    	return byteCipherData;
-    }
-    @Override
-    public byte[] transformReceiveData(DOFInterfaceID interfaceID, byte[] data)
-    {
-    	Cipher aesDecryptCipher = savedDecryptCipher;
-    	byte[] bytePlainData = aesDecryptCipher.doFinal(data);
-    	return bytePlainData;
-    }
+        public Cipher createDecryptCipher(Cipher inCipher, SecretKey sharedSecret, IvParameterSpec iv)
+        {
+        	try {
+        		Cipher aesDecryptCipher = inCipher;
+        		aesDecryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding"); //MUST specify an IV and distribute to both sides
+        		aesDecryptCipher.init(Cipher.DECRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
+        
+        		return aesDecryptCipher;
+        	}
+        	catch(Exception e) {
+        		return null;
+        	}
+        }
+        //create ciphers in initialized method or constructor - class level private variables
+        public Cipher createEncryptCipher(Cipher inCipher, SecretKey sharedSecret, IvParameterSpec iv)
+        {
+        	try {
+        		Cipher aesEncryptCipher = inCipher;
+        		aesEncryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        		aesEncryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecret, iv); //iv is the saved IV from encoded public key method
+        
+        		return aesEncryptCipher;
+        	}
+        	catch(Exception e) {
+        		return null;
+        	}
+        }
+        
+        public byte[] transformSendData(DOFInterfaceID interfaceID, byte[] data)
+        {
+        	Cipher aesEncryptCipher = savedEncryptCipher;
+        	byte[] byteCipherData = aesEncryptCipher.doFinal(data);
+        	return byteCipherData;
+        }
+        @Override
+        public byte[] transformReceiveData(DOFInterfaceID interfaceID, byte[] data)
+        {
+        	Cipher aesDecryptCipher = savedDecryptCipher;
+        	byte[] bytePlainData = aesDecryptCipher.doFinal(data);
+        	return bytePlainData;
+        }
     } //End of Data Transform
     
     private class ProviderListener extends DOFObject.DefaultProvider {
